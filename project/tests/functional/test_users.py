631ee5ced0b2545619183208dff2e6523eb02ecf
ashleyelder <shlee.elder@gmail.com>
2019-11-10 09:38:45 -0700

    gets single user with error handling

--- project/tests/functional/test_users.py
+++ project/tests/functional/test_users.py
@@ -1,7 +1,6 @@
-# project/tests/test_users.py
-
-
 import json
+from project import db
+from project.api.models import User
 
 
 def test_add_user(test_app, test_database):
@@ -18,3 +17,81 @@ def test_add_user(test_app, test_database):
     assert resp.status_code == 201
     assert 'michael@testdriven.io was added!' in data['message']
     assert 'success' in data['status']
+
+def test_add_user_invalid_json(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        '/users',
+        data=json.dumps({}),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert 'Invalid payload.' in data['message']
+    assert 'fail' in data['status']
+
+
+def test_add_user_invalid_json_keys(test_app, ):
+    client = test_app.test_client()
+    resp = client.post(
+        '/users',
+        data=json.dumps({"email": "john@testdriven.io"}),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert 'Invalid payload.' in data['message']
+    assert 'fail' in data['status']
+
+
+def test_add_user_duplicate_email(test_app, test_database):
+    client = test_app.test_client()
+    client.post(
+        '/users',
+        data=json.dumps({
+            'username': 'michael',
+            'email': 'michael@testdriven.io'
+        }),
+        content_type='application/json',
+    )
+    resp = client.post(
+        '/users',
+        data=json.dumps({
+            'username': 'michael',
+            'email': 'michael@testdriven.io'
+        }),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert 'Sorry. That email already exists.' in data['message']
+    assert 'fail' in data['status']
+
+def test_single_user(test_app, test_database):
+    user = User(username='jeffrey', email='jeffrey@testdriven.io')
+    db.session.add(user)
+    db.session.commit()
+    client = test_app.test_client()
+    resp = client.get(f'/users/{user.id}')
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert 'jeffrey' in data['data']['username']
+    assert 'jeffrey@testdriven.io' in data['data']['email']
+    assert 'success' in data['status']
+
+def test_single_user_no_id(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.get('/users/blah')
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 404
+    assert 'User does not exist' in data['message']
+    assert 'fail' in data['status']
+
+
+def test_single_user_incorrect_id(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.get('/users/999')
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 404
+    assert 'User does not exist' in data['message']
+    assert 'fail' in data['status']
